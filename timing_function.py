f6fb0c85db309a184419dd770bbd9cd73e1ee9b1
JorisMonnet <joris.monnet@free.fr>
2024-03-18 17:17:10 +0100

    First vesrion of timing function done

--- timing_function.py
+++ timing_function.py
@@ -1,26 +1,85 @@
-def timing(unperformed_midi_path: str):
+import matplotlib.pyplot as plt
+import music21
+
+
+def timing(unperformed_midi_path: str, performed_midi_path: str) -> dict and dict:
     """
     Maps symbolic time to performance attributes (tempo,velocity),
     so that one can use it to transform the "unperformed" MIDI to the "performed" MIDI.
     :param unperformed_midi_path: str
-    :return: #TODO
+    :param performed_midi_path: str
+    :return: tempo_maps, velocity_maps
     """
-    pass
+    music21_midi_unperformed = music21.converter.parse(unperformed_midi_path)
+    music21_midi_performed = music21.converter.parse(performed_midi_path)
+
+    times = {}
+    velocities = {}
+
+    for note in music21_midi_unperformed.flat.notes:
+        times[note.offset] = {"unperformed": note.duration.quarterLength, "performed": 0}
+        velocities[note.offset] = {"unperformed": note.volume.velocity, "performed": 0}
+
+    for note in music21_midi_performed.flat.notes:
+        if note.offset in times:
+            velocities[note.offset]["performed"] = note.volume.velocity
+            times[note.offset]["performed"] = note.duration.quarterLength
+
+    # Time mapping and velocity mapping are dictionaries with the same keys
+    # but different values. The keys are the time of the note and the values
+    # are dictionaries with the unperformed and performed values of the note.
+    # Now we want to create a tempo map and a velocity map from these mappings.
+    # The tempo map will have the time as key and the tempo as value.
+    # The velocity map will have the time as key and the velocity as value.
+
+    tempo_mapping = {}
+    for time in times:
+        if times[time]["performed"] != 0:
+            tempo_mapping[time] = times[time]["unperformed"] / times[time]["performed"]
+
+    velocity_mapping = {}
+    for time in velocities:
+        if velocities[time]["performed"] != 0:
+            velocity_mapping[time] = velocities[time]["unperformed"] / velocities[time]["performed"]
+
+    return tempo_mapping, velocity_mapping
 
 
-def plot_timing_as_tempo_curve(unperformed_midi_path: str):
-    """
-    Plot the function timing as a tempo curve.
-    :param unperformed_midi_path: str
-    :return: #TODO
-    """
-    pass
 
 
-def plot_timing_as_velocity_curve(unperformed_midi_path: str):
+def plot_timing_as_tempo_curve(tempo_map: dict):
     """
-    Plot the function timing as a velocity curve.
-    :param unperformed_midi_path: str
-    :return: #TODO
+    Plot the tempo_map from timing function as a tempo curve.
+    :param tempo_map: dict
+    :return: None
     """
-    pass
+    fig, ax = plt.subplots()
+    ax.plot(list(tempo_map.keys()), list(tempo_map.values()))
+    ax.set(xlabel='Time', ylabel='Tempo',
+           title='Tempo curve')
+    ax.grid()
+    ax.legend()
+    plt.show()
+
+
+def plot_timing_as_velocity_curve(velocity_map: dict):
+    """
+    Plot the velocity_map from the function timing as a velocity curve.
+    :param velocity_map: dict
+    :return: None
+    """
+    fig, ax = plt.subplots()
+    ax.plot(list(velocity_map.keys()), list(velocity_map.values()))
+    ax.set(xlabel='Time', ylabel='Velocity',
+           title='Velocity curve')
+    ax.grid()
+    ax.legend()
+    plt.show()
+
+
+# TODO remove this main, only useful for testing Task A
+if __name__ == "__main__":
+    tempo_maps, velocity_maps = timing("./asap-dataset/Chopin/Ballades/1/Ali01.mid",
+                                       "./asap-dataset/Chopin/Ballades/1/midi_score.mid")
+    plot_timing_as_tempo_curve(tempo_maps)
+    plot_timing_as_velocity_curve(velocity_maps)
