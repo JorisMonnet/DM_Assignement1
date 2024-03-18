2687ef016c5a7a76f5794f79784188e6daece111
JorisMonnet <joris.monnet@free.fr>
2024-03-18 02:06:02 +0100

    Add architecture

---
+++ timing_function.py
@@ -0,0 +1,26 @@
+def timing(unperformed_midi_path: str):
+    """
+    Maps symbolic time to performance attributes (tempo,velocity),
+    so that one can use it to transform the "unperformed" MIDI to the "performed" MIDI.
+    :param unperformed_midi_path: str
+    :return: #TODO
+    """
+    pass
+
+
+def plot_timing_as_tempo_curve(unperformed_midi_path: str):
+    """
+    Plot the function timing as a tempo curve.
+    :param unperformed_midi_path: str
+    :return: #TODO
+    """
+    pass
+
+
+def plot_timing_as_velocity_curve(unperformed_midi_path: str):
+    """
+    Plot the function timing as a velocity curve.
+    :param unperformed_midi_path: str
+    :return: #TODO
+    """
+    pass
