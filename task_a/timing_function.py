2c6db871a38892cd717c01c77d09a303ba306895
JorisMonnet <joris.monnet@free.fr>
2024-03-26 15:37:01 +0100

    Handle bR(#2), handle 6/8 with 2 or 3 downbeats (#1)

--- task_a/timing_function.py
+++ task_a/timing_function.py
@@ -8,6 +8,33 @@ by averaging the timing attributes for each beat of a meter.
 import os
 
 
+def merge_sum_and_lengths_timings(sum_and_lengths: list[dict]) -> dict:
+    """
+    Merge the sum of the durations and the number of beats for each beat of a meter of multiple files
+    :param sum_and_lengths:
+    :return: dict with meter as key and the sum of the durations and the number of beats for each beat as list
+    """
+    result = {}
+    for sum_and_lengths_file in sum_and_lengths:
+        for meter in sum_and_lengths_file:
+            if meter not in result:
+                result[meter] = {
+                    "sum_durations": [0 for _ in range(len(sum_and_lengths_file[meter]["sum_durations"]))],
+                    "number_of_beats": [0 for _ in range(len(sum_and_lengths_file[meter]["sum_durations"]))],
+                }
+            meter_for_result = meter
+            if len(result[meter]["sum_durations"]) != len(sum_and_lengths_file[meter]["sum_durations"]):
+                meter_for_result = f"{meter}_with_{str(len(sum_and_lengths_file[meter]['sum_durations']))}_db"
+                result[meter_for_result] = {
+                    "sum_durations": [0 for _ in range(len(sum_and_lengths_file[meter]["sum_durations"]))],
+                    "number_of_beats": [0 for _ in range(len(sum_and_lengths_file[meter]["sum_durations"]))],
+                }
+            for i in range(len(sum_and_lengths_file[meter]["sum_durations"])):
+                result[meter_for_result]["sum_durations"][i] += sum_and_lengths_file[meter]["sum_durations"][i]
+                result[meter_for_result]["number_of_beats"][i] += sum_and_lengths_file[meter]["number_of_beats"][i]
+    return result
+
+
 def get_sum_and_lengths_timing_one_bar(path: str) -> dict:
     """
     Get the sum of the durations and the number of beats for each beat of a meter of a single file
@@ -26,25 +53,30 @@ def get_sum_and_lengths_timing_one_bar(path: str) -> dict:
             meter = beat_type_meter_key[1] if len(beat_type_meter_key) > 1 else ""
             if meter != current_meter and meter != "":
                 current_meter = meter
-                current_int_meter = int(meter.split('/')[0])
                 if current_meter not in result:
                     result[current_meter] = {
-                        "sum_durations": [0 for _ in range(current_int_meter)],
-                        "number_of_beats": [0 for _ in range(current_int_meter)],
+                        "sum_durations": [0],
+                        "number_of_beats": [0],
                     }
                 current_beat = 0
             elif current_meter is None:
                 # Anacrusis, need to update the previous onset to get a correct first beat onset
                 previous_onset = float(line_data[0])
                 continue
+            if beat_type_meter_key[0] == "db":
+                current_beat = 0  # Downbeat
+            elif beat_type_meter_key[0] == "b":
+                current_beat += 1  # other beats
+            elif beat_type_meter_key[0] == "bR":  # Remove beats with type bR (beats that are not in the meter)
+                previous_onset = float(line_data[0])
+                continue
             new_onset = float(line_data[0]) - previous_onset
+            if len(result[current_meter]["sum_durations"]) <= current_beat:
+                result[current_meter]["sum_durations"].append(0)
+                result[current_meter]["number_of_beats"].append(0)
             result[current_meter]["sum_durations"][current_beat] += new_onset
             result[current_meter]["number_of_beats"][current_beat] += 1
             previous_onset = float(line_data[0])
-            if current_beat == current_int_meter - 1:
-                current_beat = 0  # Downbeat
-            else:
-                current_beat += 1  # Upbeats
     return result
 
 
@@ -54,28 +86,35 @@ def get_average_from_sum_and_lengths(sum_and_lengths: dict) -> dict:
     :param sum_and_lengths: dict containing the sum of the durations and the number of beats for each beat
     :return: dict with meter as key and the average timing for each beat as list
     """
-    average = {}
-    for meter in sum_and_lengths:
-        average[meter] = [sum_and_lengths[meter]["sum_durations"][i] / sum_and_lengths[meter]["number_of_beats"][i]
-                          for i in range(len(sum_and_lengths[meter]["sum_durations"]))]
-    return average
+    return {meter: [sum_and_lengths[meter]["sum_durations"][i] / sum_and_lengths[meter]["number_of_beats"][i]
+                    for i in range(len(sum_and_lengths[meter]["sum_durations"]))] for meter in sum_and_lengths}
 
 
-def get_sub_folders(parent_folder: str):
+def get_annotations_files_from_folder(folder_path: str) -> list:
     """
-    Get sub folders with the name of the parent folder.
-    :param parent_folder: Path to the parent folder.
-    :return: List of sub folders with the name of the parent folder.
+    Get the path of all the annotations files in a folder and its sub folders
+    :param folder_path: path to the folder
+    :return: list of paths to the txt files
     """
-    sub_folders = []
-    for entry in os.listdir(parent_folder):
-        full_path = os.path.join(parent_folder, entry)
-        if os.path.isdir(full_path) and entry == os.path.basename(parent_folder):
-            sub_folders.append(full_path)
-    return sub_folders
+    txt_files = []
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith("annotations.txt"):
+                txt_files.append(os.path.join(root, file))
+        for directory in dirs:
+            txt_files.extend(get_annotations_files_from_folder(os.path.join(root, directory)))
+    return txt_files
+
 
 if __name__ == "__main__":
-    symbolic = get_sum_and_lengths_timing_one_bar("./asap-dataset/Chopin/Ballades/1/midi_score_annotations.txt")
-    performance = get_sum_and_lengths_timing_one_bar("./asap-dataset/Chopin/Ballades/1/SINKEV05_annotations.txt")
-    print(get_average_from_sum_and_lengths(symbolic))
-    print(get_average_from_sum_and_lengths(performance))
+    # symbolic = get_sum_and_lengths_timing_one_bar("../asap-dataset/Bach/Fugue/bwv_846/midi_score_annotations.txt")
+    # performance = get_sum_and_lengths_timing_one_bar("../asap-dataset/Bach/Fugue/bwv_846/Shi05M_annotations.txt")
+    # print(get_average_from_sum_and_lengths(symbolic))
+    # print(get_average_from_sum_and_lengths(performance))
+
+    files_list = get_annotations_files_from_folder("../asap-dataset/")
+    perf_files = [file for file in files_list if "midi_score_annotations.txt" not in file]
+    sum_and_lengths_list = [get_sum_and_lengths_timing_one_bar(file) for file in perf_files]
+    merged_sum_and_lengths = merge_sum_and_lengths_timings(sum_and_lengths_list)
+    average = get_average_from_sum_and_lengths(merged_sum_and_lengths)
+    print(average)
